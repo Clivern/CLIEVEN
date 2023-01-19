d6b70b6f4053f93e623316f4d3a75852c76e4254
Clivern <hello@clivern.com>
2023-01-19 15:42:51 +0200

    Rust: Add day3

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -2,8 +2,30 @@ fn main() {
     let msg = "Hello, World!";
     println!("{}", msg);
     println!("{}", reverse_string(msg));
+    println!("{}", hello());
 }
 
 fn reverse_string(input: &str) -> String {
     String::from(input).chars().rev().collect()
+}
+
+fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::hello;
+    use crate::reverse_string;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, World!");
+    }
+
+    #[test]
+    fn test_reverse_string() {
+        assert_eq!(reverse_string("Hello"), String::from("olleH"));
+    }
 }
